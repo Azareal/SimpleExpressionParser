fb059fd463aa0d424a7b9ebdb538c32b9bbb0c07
Azareal <azareal@users.noreply.github.com>
2017-04-02 08:57:23 +0100

    Moved the arbitrary function resolver from the SajuukBot / AzaBot repo to SEP.

--- arbitraryFunctions.go
+++ arbitraryFunctions.go
@@ -1,7 +1,9 @@
 package sep
 
+import "strings"
 import "strconv"
 import "time"
+import "errors"
 
 var arbitraryFunctions map[string]func(...string)(string,error) = map[string]func(...string)(string,error){
 	"unixtime": func(_ ...string) (string, error) {
@@ -23,4 +25,29 @@ func SetArbitraryFunction(name string, callback func(...string)(string,error), p
 func HasArbitraryFunction(name string) bool {
 	_, ok := arbitraryFunctions[name]
 	return ok
+}
+
+
+func ResolveArbitraryFunction(name string, paramstr string, server_data Datastore, depth int) (result string, err error) {
+	callback, ok := arbitraryFunctions[name]
+	if !ok {
+		return "", errors.New("this function doesn't exist x.x")
+	}
+	
+	params := strings.Split(paramstr,",")
+	if arbitraryFunctionParams[name] == 0 {
+		return callback(params...)
+	}
+	
+	/*for _, param := range params {
+		if param[0] == '*' {
+			
+		} else if param[0] == '"' {
+			
+		} else {
+			
+		}
+	}*/
+	
+	return ":soontm:", nil
 }
